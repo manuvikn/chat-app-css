a0f42e59f54e9524e2db5ce096a74b47c5a45402
Manuel Victoria <manuelvictoriacode@gmail.com>
2024-05-12 15:51:33 +0200

    angular v17

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,21 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, 'tsconfig.json'),
-  [/* mapped paths to share */]);
+module.exports = withModuleFederationPlugin({
+  name: "chatAppCss",
 
-module.exports = {
-  output: {
-    uniqueName: "chatAppCss",
-    publicPath: "auto"
+  exposes: {
+    // Update this whole line (both, left and right part):
+    "./ChatAppCssModule": "./src/app/chat-app-css/chat-app-css.module.ts",
   },
-  optimization: {
-    runtimeChunk: false
-  },   
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
-  },
-  experiments: {
-    outputModule: true
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-        library: { type: "module" },
 
-        // For remotes (please adjust)
-        name: "chatAppCss",
-        filename: "remoteEntry.js",
-        exposes: {
-            './ChatAppCssModule': './/src/app/chat-app-css/chat-app-css.module.ts',
-        },        
-      
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-
-          ...sharedMappings.getDescriptors()
-        })
-        
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
     }),
-    sharedMappings.getPlugin()
-  ],
-};
+  },
+});
